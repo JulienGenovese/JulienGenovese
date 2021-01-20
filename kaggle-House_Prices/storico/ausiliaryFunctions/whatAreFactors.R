09dcf1372980d512c2d370a6cbf9e663c1e6fd4d
Julien Genovese <juliengenovese@hotmail.it>
2021-01-20 08:43:24 +0100

    Cleaning github

--- kaggle-House_Prices/storico/ausiliaryFunctions/whatAreFactors.R
+++
@@ -1,7 +0,0 @@
-whatAreFactors<-function(dataset){
-  namesFactor<-c()
-  for(i in 1:ncol(dataset)){
-    if(is.factor(dataset[,i])) namesFactor<-c(namesFactor, colnames(dataset)[i])
-  }
-  return(namesFactor)
-}
